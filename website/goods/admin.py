a46d0aaed1ccab43a55ceba93baa44d11a058bf3
Sergey Borodavka <94472325+DrBATCOH@users.noreply.github.com>
2022-01-12 16:47:40 +0300

    update admin

--- website/goods/admin.py
+++ website/goods/admin.py
@@ -4,5 +4,12 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post_title', 'post_description', 'publish', 'status')
+    list_filter = ('status', 'post_date', 'publish', 'author')
+    search_fields = ('post_title', 'post_description')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
--- website/goods/models.py
+++ website/goods/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    author = models.ForeignKey(User,null=True, related_name='posts',on_delete=models.CASCADE)
     post_title = models.CharField(blank=True, max_length=100)
     post_description = models.TextField(max_length=400)
     post_image = models.FileField(upload_to='img/')
+    publish = models.DateTimeField(default=timezone.now)
     post_date = models.DateField(null=True, auto_now_add="True")
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     def __str__(self):
         return self.post_title
@@ -25,4 +34,4 @@ class Comment(models.Model):
 
     class Meta:
         verbose_name = 'Комментарий'
-        verbose_name_plural = 'Коментарии'
+        verbose_name_plural = 'Комментарии'
